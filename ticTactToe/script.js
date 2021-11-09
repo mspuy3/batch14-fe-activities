ce8ff887e1eb57973562c303464ab8b7c6d68ca9
Michael Siemens Uy <89561283+mspuy3@users.noreply.github.com>
2021-11-09 22:24:04 +0800

    Added next and previous in a game. Changed state setter to boardStatus instead of displayed HTML element.

--- ticTactToe/script.js
+++ ticTactToe/script.js
@@ -1,3 +1,15 @@
+let gameRecord = {
+   numberOfGames: 0,
+   gameHistory: [],
+   xPlayer: "X",
+   xPlayerWins: 0,
+   xPlayerWonGames:[],
+   xPlayer: "X",
+   xPlayerWins: 0,
+   xPlayerWonGames:[],
+};
+
+
 let boardStatus = [
    ['','',''],
    ['','',''],
@@ -5,6 +17,12 @@ let boardStatus = [
 ]
 
 let boardHistory = [];
+
+let boardHistoryViewer = 1;
+
+
+
+let boardLatest = boardHistory[boardHistory.length - 1];
 
 let winnerSwitch = 0;
 
@@ -22,10 +40,35 @@ function playerToggler(playingPiece) {
 
 
 
-function boardUpdater(pieceClickedElement,playingPiece){
+function boardStatusUpdater(pieceClickedElement,playingPiece){
    boardStatus[pieceClickedElement[1]][pieceClickedElement[3]] = playingPiece;
+   console.log("This is the current board");
    console.log(boardStatus);
 }
+
+
+
+
+
+function boardHTMLUpdater(chosenBoard){
+
+   let rowHTML = 0;
+   let colHTML = 0;
+
+   chosenBoard.forEach(row => {
+      row.forEach(piece => {
+
+         let tileHTML = "r"+ String(rowHTML) +"c"+ String(colHTML);
+         let tileContent = document.getElementById(tileHTML);
+         tileContent.textContent = piece;
+         
+         colHTML += 1;});
+
+      rowHTML += 1;
+      colHTML -= 3;}
+   )
+
+};
 
 
 
@@ -35,18 +78,25 @@ function warningToggler(clicked) {
    var element = document.getElementById("warningBanner");
 
    if (element.classList.contains("warningHide") && clicked === ''){
-      return
-   }
+      //  return
+       console.log("Empty tile! Successful process!");
+    }
 
    else if (element.classList.contains("warningHide") && clicked !== ''){
-      console.log("The tile is taken!");
-      
+      console.log("The tile is taken! I'll show the banner");
+      element.classList.remove("warningHide"); 
    } 
    
-   else if (element.classList.contains("") && clicked !== '') {
-      element.classList.toggle("warningHide");
-      console.log("The tile is taken!");
-   } 
+   else if (!element.classList.contains("warningHide") && clicked === ''){
+      //  return
+       console.log("Empty tile! I'll remove the banner");
+       element.classList.add("warningHide"); 
+    }
+
+   else if (!element.classList.contains("warningHide") && clicked !== ''){
+   //  return
+      console.log("The tile is taken! I'll retain the banner");
+   }
        
 }
 
@@ -100,31 +150,39 @@ function theChecker(playingPiece, boardStatus) {
 
 function myFunction(pieceClicked) {
 
-   pieceClickedElement = pieceClicked.target.id;
+   let pieceClickedElement = pieceClicked.target.id;
    let playingPiece = document.querySelector('#currentPlayer').textContent;
 
-   if(pieceClicked.target.textContent === '' && winnerSwitch == 0) {
+   let tileChecker = boardStatus[pieceClickedElement[1]][pieceClickedElement[3]];
+
+   if(tileChecker === '' && winnerSwitch == 0) {
    
+      console.clear();
       // console.log(pieceClicked.target.textContent);
       // console.log(pieceClickedElement);
       // console.log(playingPiece);
       
-      boardUpdater(pieceClickedElement,playingPiece);
+   
+      warningToggler(tileChecker);
+
+      boardStatusUpdater(pieceClickedElement,playingPiece);
 
       boardHistory.push(JSON.parse(JSON.stringify(boardStatus)));
-      console.log(boardHistory);
+      console.log("This is the board history");
+      console.log( boardHistory);
    
-      document.getElementById(String(pieceClickedElement)).textContent = playingPiece;
-   
-      playerToggler(playingPiece);
+      // document.getElementById(String(pieceClickedElement)).textContent = playingPiece;
+      boardHTMLUpdater(boardHistory[boardHistory.length - 1]);
+
 
       theChecker(playingPiece, boardStatus);
+      // theChecker(playingPiece, boardHistory[boardHistory.length - 1]);
       
-      warningToggler(pieceClicked.target.textContent);
+      playerToggler(playingPiece);      
 
    } else {
 
-      warningToggler(pieceClicked.target.textContent);
+      warningToggler(tileChecker);
 
    }
 
@@ -135,3 +193,54 @@ function myFunction(pieceClicked) {
 
 
 document.querySelectorAll('.piece').forEach(piece => piece.addEventListener('click', myFunction));
+
+
+
+
+function previousBoard () {
+   console.clear();
+   
+   boardHistoryViewer += 1;
+   let boardHistoryViewed = boardHistory[boardHistory.length - boardHistoryViewer];
+   console.log(boardHistory.length - boardHistoryViewer);
+   console.log(boardHistory);
+   console.log(boardHistoryViewed);
+   boardHTMLUpdater(boardHistoryViewed);
+
+
+}
+
+function currentBoard() {
+   console.clear();
+   boardHTMLUpdater(boardStatus);
+   boardHistoryViewer = 1;
+
+
+   console.log("This is the current board");
+   console.log(boardStatus);
+   console.log("This is the board history");
+   console.log( boardHistory);
+
+   warningToggler('');
+}
+
+function nextBoard () {
+   console.clear();
+   
+   boardHistoryViewer -= 1;
+   let boardHistoryViewed = boardHistory[boardHistory.length - boardHistoryViewer];
+   console.log(boardHistory.length - boardHistoryViewer);
+   console.log(boardHistory);
+   console.log(boardHistoryViewed);
+   boardHTMLUpdater(boardHistoryViewed);
+
+
+
+}
+
+
+document.querySelector('#previousButton').addEventListener('click',previousBoard);
+
+document.querySelector('#currentButton').addEventListener('click',currentBoard);
+
+document.querySelector('#nextButton').addEventListener('click',nextBoard);
